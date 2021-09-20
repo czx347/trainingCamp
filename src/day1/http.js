8785c77d064dfd17a3b8861d879be2752c7ef13a
347 <948773471@qq.com>
2021-09-20 11:43:45 +0800

    完善http

--- src/day1/http.js
+++ src/day1/http.js
@@ -2,12 +2,25 @@ const fs = require('fs');
 const http = require('http')
 
 http.createServer((request, response) => {
-  fs.readFile('src/day1/index.html',(err, data) => {
-    if(err) {
-      console.log(err);
-    }
-    response.end(data);
-  } )
+  const {url, method} = request;
+  if(url === '/' && method === 'GET') {
+    fs.readFile('src/day1/index.html',(err, data) => {
+      if(err) {
+        response.writeHead(500, {
+          'Content-Type': 'text/plain;charset=utf-8'
+        }) = 500;
+        response.end("500 Server挂了!");
+      }
+      response.statusCode = 200;
+      response.setHeader('Content-Type', 'text/html');
+      response.end(data);
+    } )
+  }else {
+    response.statusCode = 404;
+    response.setHeader('Content-Type', 'text/html');
+    response.end("404 NotFound");
+  }
+  
 }).listen(3000, () => {
   console.log("server is running on http://localhost:3000")
 })
